922e953d29b9ec167b69dfb09f6b67d3304159a4
Michael Mizner <michaelmizner@gmail.com>
2016-07-22 06:32:19 -0400

    initial

---
+++ entry.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Woocommerce Orders & Scheduling
+Plugin URI:  https://github.com/Mizner/woocommerce-orders-and-scheduling.git
+Description: Split payment of a product up between multiple users/dates
+Version:     0.0.1
+Author:     Michael Mizner <michaelmizner@gmail.com>
+Author URI:  https://github.com/Mizner
+License:     MIT
+License URI: https://opensource.org/licenses/MIT
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
